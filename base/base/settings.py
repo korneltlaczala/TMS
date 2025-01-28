9afacf885f85b551dd4c46cbee4136bf9c2424c5
korneltlaczala <kornel.tlaczala@gmail.com>
2025-01-28 12:35:04 +0100

    email confirmation finally works

--- base/base/settings.py
+++ base/base/settings.py
@@ -128,14 +128,15 @@ STATICFILES_DIRS = [
 ]
 
 import os
-from dotenv import load_dotenv
-load_dotenv(os.path.join(BASE_DIR, '..', '.env'))
+from decouple import config
+
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_USE_TLS = True
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
-EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER')
-EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD')
+EMAIL_HOST_USER = config('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')
+DEFAULT_FROM_MAIL = config('EMAIL_HOST_USER')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
